be0bc9ac00ea8f5b21c6bfb4c5c426d16a10e01f
Sinchana41 <sinchanaan1234@gmail.com>
2026-02-14 15:31:37 +0530

    Generic QuantityLength with enum and conversion logic

---
+++ QuantityMeasurementApp/Models/QuantityLength.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantityMeasurementApp.Models
+{
+    public class QuantityLength
+    {
+
+        private readonly double _value;
+        private readonly LengthUnit _unit;
+
+        private const double InchToFeet = 1.0 / 12.0;
+
+        public QuantityLength(double value, LengthUnit unit)
+        {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                throw new ArgumentException("Invalid numeric value.");
+
+            _value = value;
+            _unit = unit;
+        }
+
+        public double Value => _value;
+        public LengthUnit Unit => _unit;
+
+        private double ConvertToFeet()
+        {
+            return _unit switch
+            {
+                LengthUnit.Feet => _value,
+                LengthUnit.Inch => _value * InchToFeet,
+                _ => throw new ArgumentException("Unsupported unit"
+                )
+            };
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (this == obj)
+                return true;
+
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            var other = (QuantityLength)obj;
+
+            return this.ConvertToFeet()
+               .CompareTo(other.ConvertToFeet()) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return ConvertToFeet().GetHashCode();
+        }
+
+    }
+}
---
+++ QuantityMeasurementAppTest/QuantityLengthTests.cs
@@ -0,0 +1,76 @@
+﻿using QuantityMeasurementApp.Models;
+
+namespace QuantityMeasurementAppTest
+{
+    public class QuantityLengthTests
+    {
+        [Test]
+        public void TestEquality_FeetToFeet_SameValue()
+        {
+            var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+            var q2 = new QuantityLength(1.0, LengthUnit.Feet);
+
+            Assert.That(q1.Equals(q2), Is.True);
+        }
+
+        [Test]
+        public void TestEquality_InchToInch_SameValue()
+        {
+            var q1 = new QuantityLength(1.0, LengthUnit.Inch);
+            var q2 = new QuantityLength(1.0, LengthUnit.Inch);
+
+            Assert.That(q1.Equals(q2), Is.True);
+        }
+
+        [Test]
+        public void TestEquality_FeetToInch_Equivalent()
+        {
+            var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+            var q2 = new QuantityLength(12.0, LengthUnit.Inch);
+
+            Assert.That(q1.Equals(q2), Is.True);
+        }
+
+        [Test]
+        public void TestEquality_InchToFeet_Equivalent()
+        {
+            var q1 = new QuantityLength(12.0, LengthUnit.Inch);
+            var q2 = new QuantityLength(1.0, LengthUnit.Feet);
+
+            Assert.That(q1.Equals(q2), Is.True);
+        }
+
+        [Test]
+        public void TestEquality_DifferentValue()
+        {
+            var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+            var q2 = new QuantityLength(2.0, LengthUnit.Feet);
+
+            Assert.That(q1.Equals(q2), Is.False);
+        }
+
+        [Test]
+        public void TestEquality_NullComparison()
+        {
+            var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+
+            Assert.That(q1.Equals(null), Is.False);
+        }
+
+        [Test]
+        public void TestEquality_SameReference()
+        {
+            var q1 = new QuantityLength(1.0, LengthUnit.Feet);
+
+            Assert.That(q1.Equals(q1), Is.True);
+        }
+
+        [Test]
+        public void Test_InvalidValue_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new QuantityLength(double.NaN, LengthUnit.Feet));
+        }
+    }
+}
+
